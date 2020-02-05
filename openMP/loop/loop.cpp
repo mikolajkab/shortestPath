53bde9f3a2ff6879d8056c61b5a5eae9bfe55a5a
mik <mikolaj.kabacinski@gmail.com>
2020-02-05 10:32:22 +0100

    refactor folder structure

---
+++ openMP/loop/loop.cpp
@@ -0,0 +1,30 @@
+ #include <omp.h>
+ #include <stdio.h>
+
+ #define N 100
+ #define CHUNKSIZE 2
+
+ int main(int argc, char *argv[]) {
+
+ int i, chunk, tid;
+ float a[N], b[N], c[N];
+
+ /* Some initializations */
+ for (i=0; i < N; i++)
+   a[i] = b[i] = i * 1.0;
+ chunk = CHUNKSIZE;
+
+ omp_set_num_threads(5);
+ #pragma omp parallel shared(a,b,c,chunk) private(i, tid)
+   {
+   tid = omp_get_thread_num();
+
+   #pragma omp for schedule(static, chunk) nowait
+   for (i=0; i < N; i++)
+   {
+     c[i] = a[i] + b[i];
+     printf("thread id = %d i = %d\n", tid, i);
+   }
+   }   /* end of parallel region */
+
+ }
